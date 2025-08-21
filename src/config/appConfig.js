09592208e33fb8d26f71e2dcd44ccd74329d6d3c
Lakmal Wijewardene <lakishwijewardene@gmail.com>
2025-08-21 10:14:03 +1000

    chore: scaffold app (Vite + React 18 + Semantic UI + config)

---
+++ src/config/appConfig.js
@@ -0,0 +1,1 @@
+export const APP_CONFIG={apiBase:import.meta.env.VITE_API_BASE_URL||'http://localhost:3000',features:{newsletter:true}};
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import 'semantic-ui-css/semantic.min.css';
+import './theme.css';
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
